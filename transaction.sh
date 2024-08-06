d8952c9bc339cde9b295a9e352d67ee3a5e1d2d3
Arijit <arijit.ghosh@quadbtech.com>
2024-08-06 17:06:10 +0530

    add tokes creation

---
+++ transaction.sh
@@ -0,0 +1,4 @@
+dfx canister call icrc1_ledger_backend icrc1_transfer "(record { 
+    to = record { owner = principal \"qlwqy-ubcd7-suaz2-bnr3l-3haxg-ywugy-zcvq5-epsfq-em324-phchq-xae\"; subaccount = null }; 
+    amount = 300000 : nat 
+})"
